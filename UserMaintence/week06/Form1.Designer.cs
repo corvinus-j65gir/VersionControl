96c1d9cdb6888f13f5b4fdbdf5690b78bb2012b3
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 04:18:05 +0100

    6/14

--- UserMaintence/week06/Form1.Designer.cs
+++ UserMaintence/week06/Form1.Designer.cs
@@ -36,11 +36,17 @@
             this.button2 = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.button6);
+            this.mainPanel.Controls.Add(this.button5);
+            this.mainPanel.Controls.Add(this.button4);
             this.mainPanel.Controls.Add(this.button3);
             this.mainPanel.Controls.Add(this.lblNext);
             this.mainPanel.Controls.Add(this.button2);
@@ -74,7 +80,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(149, 26);
+            this.button2.Location = new System.Drawing.Point(146, 26);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 72);
             this.button2.TabIndex = 1;
@@ -85,7 +91,7 @@
             // lblNext
             // 
             this.lblNext.AutoSize = true;
-            this.lblNext.Location = new System.Drawing.Point(263, 56);
+            this.lblNext.Location = new System.Drawing.Point(348, 26);
             this.lblNext.Name = "lblNext";
             this.lblNext.Size = new System.Drawing.Size(65, 13);
             this.lblNext.TabIndex = 2;
@@ -94,12 +100,39 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(254, 107);
+            this.button3.Location = new System.Drawing.Point(146, 104);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 72);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "PRESENT";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(249, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button6.Location = new System.Drawing.Point(249, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -124,6 +157,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
     }
 }
 
---
+++ UserMaintence/week06/entities/Present.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week06.Abstractions;
+
+namespace week06.entities
+{
+    public class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
