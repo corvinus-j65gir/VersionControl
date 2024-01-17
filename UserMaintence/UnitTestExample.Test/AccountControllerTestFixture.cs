a4af6e1cbd61faa0f2b6431f235baefebc7759bd
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 05:46:44 +0100

    8/12

--- UserMaintence/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UserMaintence/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -63,7 +63,12 @@ namespace UnitTestExample.Test
         public void TestRegisterHappyPath(string email, string password)
         {
             // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Returns<Account>(a => a);
             var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
 
             // Act
             var actualResult = accountController.Register(email, password);
@@ -72,6 +77,7 @@ namespace UnitTestExample.Test
             ClassicAssert.AreEqual(email, actualResult.Email);
             ClassicAssert.AreEqual(password, actualResult.Password);
             ClassicAssert.AreNotEqual(Guid.Empty, actualResult.ID);
+            accountServiceMock.Verify(m => m.CreateAccount(actualResult), Times.Once);
         }
 
         [
@@ -98,6 +104,35 @@ namespace UnitTestExample.Test
             catch (Exception ex)
             {
                 ClassicAssert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234")
+        ]
+
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                ClassicAssert.IsInstanceOf<ApplicationException>(ex);
             }
 
             // Assert
