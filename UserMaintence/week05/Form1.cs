63efaa0126dc0f277d74ca4e3c215ad760fb9263
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 02:53:14 +0100

    5/7

--- UserMaintence/week05/Form1.cs
+++ UserMaintence/week05/Form1.cs
@@ -16,6 +16,7 @@ namespace week05
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
 
         public Form1()
         {
@@ -36,8 +37,6 @@ namespace week05
             var result = response.GetExchangeRatesResult;
         }
         
-        
-
         private void RefreshData()
         {
             Rates.Clear();
@@ -67,6 +66,39 @@ namespace week05
                     rate.Value = value / unit;
                 }
             }
+        }
+
+        private void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = series.ChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 
