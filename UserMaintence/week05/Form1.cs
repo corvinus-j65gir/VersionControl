22c511eedfaff999b97e05038b430abad2757f50
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 02:19:12 +0100

    5/3

--- UserMaintence/week05/Form1.cs
+++ UserMaintence/week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.MnbServiceReference;
 
 namespace week05
 {
@@ -15,6 +16,26 @@ namespace week05
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
         }
+
+
     }
+
 }
