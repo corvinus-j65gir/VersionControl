2dc9d509851e37ec58910970b2b9dadff9f92fc7
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 03:36:30 +0100

    6/9

--- UserMaintence/week06/Abstractions/IToyFactory.cs
+++ UserMaintence/week06/Abstractions/IToyFactory.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using week06.entities;
 
 namespace week06.Abstractions
 {
     public interface IToyFactory
     {
         Toy CreateNew();
+    }
+
+    public class BallFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+
+    public class CarFactory : IToyFactory 
+    {
+        public Toy CreateNew() 
+        {
+            return new Car();
+        }
     }
 }
