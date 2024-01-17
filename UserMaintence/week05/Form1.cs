8bbdd537f497c99b4a96f227b9a20fc46dfcda37
Péter Tamás Weber <peter.weber@stud.uni-corvinus.hu>
2024-01-17 02:33:40 +0100

    5/4

--- UserMaintence/week05/Form1.cs
+++ UserMaintence/week05/Form1.cs
@@ -7,18 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 using week05.MnbServiceReference;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+        }
 
+        private void GetExchangeRates()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
-
             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = "EUR",
@@ -26,16 +31,17 @@ namespace week05
                 endDate = "2020-06-30"
             };
 
-            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
-            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
             var response = mnbService.GetExchangeRates(request);
-
-            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
-            // Ezért a result változó valójában string típusú.
             var result = response.GetExchangeRatesResult;
         }
 
+        private void RefreshData()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates;
 
+
+        }
     }
 
 }
